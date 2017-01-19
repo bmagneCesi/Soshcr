1dd113ad141024275edaeaff61f095af22543441
Matthias <matthiasou@yahoo.fr>
2017-01-19 10:18:49 +0100

    Classification..

--- classification.php
+++ classification.php
@@ -1,6 +1,48 @@
 <!-- HEADER -->
 <?php include('header.php') ?>
 <!-- HEADER -->
+
+<?php
+include("connexion.php");
+$pdo = connect();
+
+$utilisateurs=$pdo->query("SELECT * FROM utilisateur");
+$utilisateurs->setFetchMode(PDO::FETCH_OBJ);
+while( $utilisateur = $utilisateurs->fetch() )
+{
+	//echo 'NomUser : '.$utilisateur->nom.'<br>';
+	//echo 'Longitude : '.$utilisateur->longitude.'<br>';
+	//echo 'Latitude : '.$utilisateur->latitude.'<br>';
+	$long  =  $_SESSION['longitudeVille'];
+	$lat  =  $_SESSION['latitudeVille'];
+	$formule="(6366*ACOS(COS(RADIANS($lat))*COS(RADIANS(latitude))*COS(RADIANS(longitude)-RADIANS($long))+SIN(RADIANS($lat))*SIN(RADIANS(latitude))))";
+	$resultats=$pdo->query("SELECT *, COUNT(*) as c, ".$formule." AS dist FROM utilisateur WHERE ".$formule." < $utilisateur->nombre_kilometre ORDER BY dist ASC");
+	$resultats->setFetchMode(PDO::FETCH_OBJ);
+	while( $resultat = $resultats->fetch() )
+	{
+
+
+		if ($resultat->nom == $utilisateur->nom){
+			//echo 'UtilisateurBon : '.$resultat->nom.'<br>';
+			echo "Le nombre de personne est de : ".$resultat->c;
+		}
+
+	}
+}
+$utilisateurs->closeCursor();
+
+
+
+
+
+if ( isset($_POST['classification']) ){
+
+	$_SESSION['classification'] = $_POST['classification'];
+	header('Location: secteur_activite.php');
+
+}
+?>
+
 	<ul>
 		<li>
 			<form method="post">
@@ -44,21 +86,6 @@
 <input type="button" value="Retour" onclick="document.location.href='ville.php';">
 
 
-<?php
-include("connexion.php");
-$pdo = connect();
-
-
-
-
-
-if ( isset($_POST['classification']) ){
-
-	$_SESSION['classification'] = $_POST['classification'];
-	header('Location: secteur_activite.php');
-
-}
-?>
 
 
 <!-- FOOTER -->
--- secteur_activite.php
+++ secteur_activite.php
@@ -5,6 +5,49 @@
 <?php print_r($_SESSION); ?>
 
 
+<?php
+include("connexion.php");
+$pdo = connect();
+
+$utilisateurs=$pdo->query("SELECT * FROM utilisateur");
+$utilisateurs->setFetchMode(PDO::FETCH_OBJ);
+while( $utilisateur = $utilisateurs->fetch() )
+{
+    //echo 'NomUser : '.$utilisateur->nom.'<br>';
+    //echo 'Longitude : '.$utilisateur->longitude.'<br>';
+    //echo 'Latitude : '.$utilisateur->latitude.'<br>';
+    $long  =  $_SESSION['longitudeVille'];
+    $lat  =  $_SESSION['latitudeVille'];
+    $classification = $_SESSION['classification'];
+    $formule="(6366*ACOS(COS(RADIANS($lat))*COS(RADIANS(latitude))*COS(RADIANS(longitude)-RADIANS($long))+SIN(RADIANS($lat))*SIN(RADIANS(latitude))))";
+    $resultats=$pdo->query("SELECT *, COUNT(*) as c, ".$formule." AS dist FROM utilisateur, poste_recherche_has_experience WHERE utilisateur.id_utilisateur=poste_recherche_has_experience.utilisateur_id_utilisateur AND ".$formule." < $utilisateur->nombre_kilometre AND etablissement_id_etablissement=$classification  ORDER BY dist ASC");
+    $resultats->setFetchMode(PDO::FETCH_OBJ);
+    while( $resultat = $resultats->fetch() )
+    {
+
+
+        if ($resultat->nom == $utilisateur->nom){
+            //echo 'UtilisateurBon : '.$resultat->nom.'<br>';
+            echo "Le nombre de personne est de : ".$resultat->c;
+        }
+
+    }
+}
+$utilisateurs->closeCursor();
+
+
+
+
+
+if ( isset($_POST['secteur_activite']) ){
+
+   // $_SESSION['secteur_activite'] = $_POST['secteur_activite'];
+   // header('Location: secteur_activite.php');
+
+}
+?>
+
+
 
 <!-- FOOTER -->
 <?php include('footer.php') ?>
